541457595816b84d51b9f64685e607b05aebb652
jbmjbmjbm <45790129+jbmjbmjbm@users.noreply.github.com>
2023-11-12 12:16:58 -0300

    2º commit DB

--- db.sql
+++ db.sql
@@ -1,14 +1,18 @@
+-- criando a Banco de Dados Curso De Programacao
 create database CursoDeProgramacao;
 
+
+-- selecionando o banco de dados CursosDeProgramação 
 Use CursoDeProgramacao;
 
+-- criando a tabela disciplinas
 create table disciplinas(
 id_disciplina int auto_increment primary key,
 nome_disciplina varchar(50) not null,
 nome_professor varchar(50) not null
 )
 
-
+-- criando a tabela aluno
 create table alunos (
 	id_alunos int auto_increment primary key,
     nome_alunos varchar(50) not null,
@@ -18,6 +22,8 @@ create table alunos (
     
 )
 
+
+-- inserindo dados nas tabelas disciplinas e alunos
 insert into disciplinas(nome_disciplina, nome_professor) values ('Bancos de dados', 'Maria Alves');
 insert into disciplinas(nome_disciplina, nome_professor) values ('Python', 'Pietro Souza');
 insert into disciplinas(nome_disciplina, nome_professor) values ('POO', 'Bia Tavares');
@@ -30,14 +36,40 @@ insert into alunos(nome_alunos, disciplinas_id) values ('Rian', null );
 insert into alunos(nome_alunos, disciplinas_id) values ('Mia', 2 );
 insert into alunos(nome_alunos, disciplinas_id) values ('Malu', 1 );
 
+
+-- select
 select * from disciplinas;
 select * from alunos;
 
+-- inner join
 select nome_alunos, nome_disciplina from alunos
 inner join disciplinas
 on disciplinas.id_disciplina = alunos.disciplinas_id;
  
+
+
+-- left join
  select nome_alunos, nome_disciplina
  from
  alunos left join disciplinas
+ on disciplinas.id_disciplina = alunos.disciplinas_id;
+ 
+ 
+-- right join 
+select nome_alunos, nome_disciplina
+ from
+ alunos right join disciplinas
+ on disciplinas.id_disciplina = alunos.disciplinas_id;
+ 
+ 
+ 
+ /*---Union---*/
+  select nome_alunos, nome_disciplina
+ from
+ alunos left join disciplinas
+ on disciplinas.id_disciplina = alunos.disciplinas_id
+union 
+select nome_alunos, nome_disciplina
+ from
+ alunos right join disciplinas
  on disciplinas.id_disciplina = alunos.disciplinas_id;
