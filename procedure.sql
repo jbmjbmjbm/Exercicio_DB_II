d78fe78cec4d1b42fa3a7475a66996301f5566af
jbmjbmjbm <45790129+jbmjbmjbm@users.noreply.github.com>
2023-11-13 16:29:11 -0300

    7º commit Procedure

--- procedure.sql
+++ procedure.sql
@@ -1,6 +1,8 @@
+Procedure oriunda da tabela venda diaria do Banco de Dados Empresa.
+
 delimiter //
 create procedure produtos()
 begin
 	select produto from vendas where data_venda_diaria = data_venda;
 end //
-delimiter;  
+delimiter;
